d4e9f6bc4f51ffd0c3b0290fbaffb42443ceea92
gmemstr <bladesimmer@gmail.com>
2017-10-01 19:31:52 -0700

    Add comments

--- webserver.py
+++ webserver.py
@@ -4,11 +4,16 @@ import MySQLdb
 import calendar, time
 import json
 
+
+# Main handler, simply returns index.html
 class MainHandler(tornado.web.RequestHandler):
 
 	def get(self):
 		self.render("index.html")
 
+# Return "assets" aka javascript and css files. 
+# Uses Content-Type of CSS because CSS won't work
+# without it and JavaScript doesn't seem affected
 class ResourceHandler(tornado.web.RequestHandler):
 
 	def get(self, source):
@@ -16,12 +21,16 @@ class ResourceHandler(tornado.web.RequestHandler):
 		self.set_header("Content-Type", 'text/css; charset="utf-8"')
 		self.write(resource)
 
+# Fetch, parse and return data from database
 class DataHandler(tornado.web.RequestHandler):
 
 	def get(self, coin, change):
+		# Establish connection
 		self.db = MySQLdb.connect('localhost', 'coingraphs', 'CGpassword', 'coingraphs')
 		c  = self.db.cursor()
 		finish = True
+
+		# "Hacky" sanitization of inputs
 		if coin != "eth" and coin != "btc": # Quick dirty workaround for sanitization
 			self.write('{"message":"invalid_coin"}')
 			finish = False
@@ -30,6 +39,7 @@ class DataHandler(tornado.web.RequestHandler):
 			finish = False
 
 		if finish == True:
+			# Sanitization is fine, do query
 			q = "SELECT 'time', `%s` AS price FROM `%s` WHERE time > %%s ORDER BY time ASC" % (change, coin)
 			c.execute(q % (calendar.timegm(time.gmtime()) - 36000))
 
@@ -37,18 +47,20 @@ class DataHandler(tornado.web.RequestHandler):
 			data = c.fetchall()
 
 			json_data = []
-			for row in data:
+			for row in data: # Append rows to data array
 				json_data.append(dict(zip(row_headers,row)))
 
 			self.db.close()
 
-			self.write('{"'+coin+'":'+json.dumps(json_data)+"}")
+			# Weird little prefix/suffix because of how main.js
+			# already parses JSON from PHP
+			self.write('{"'+coin+'":'+json.dumps(json_data)+"}") 
 
 
 def make_app():
 
 	settings = {
-		"debug": True
+		"debug": True # Probably remove this in production, allows live code reloading
 	}
 
 	# Build our routes
@@ -60,7 +72,7 @@ def make_app():
 
 def run_app():
 	app = make_app()
-	app.listen(8080)
+	app.listen(8080) # Can run on port 80 but requires sudo on *nix
 	print("Webserver listening on port 8080")
 
 	tornado.ioloop.IOLoop.current().start()
