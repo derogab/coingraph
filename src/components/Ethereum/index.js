c4f4dd28d7a3c532d5725095a9c40ac98eb78fcc
Riccardo Di Benedetto <dibe.ricky@gmail.com>
2019-10-09 00:16:15 +0200

    created Coin component

--- src/components/Ethereum/index.js
+++ src/components/Ethereum/index.js
@@ -1,54 +1,16 @@
 import React from 'react';
-import { Row, Col } from 'react-bootstrap';
-import {
-  LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer
-} from 'recharts';
-import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Coin from '../Coin'
 import './index.css';
 
 
+
 function Ethereum(props) {
-
   return (
-    <div className="Ethereum">
-      <header className="Ethereum-header">
-      </header>
-      <div className="Ethereum-body">
-
-      <Row>
-        <Col className="with-border set-height">
-          <ResponsiveContainer>
-            <LineChart margin={{ top: 5, right: 0, left: 0, bottom: 5 }} data={props.datagraph}>
-              <Line type='monotone' dataKey='pv' stroke='#8884d8' strokeWidth={2} />
-            </LineChart>
-          </ResponsiveContainer>
-        </Col>
-        <Col>
-          <Row>
-            <Col className="with-border value-space">
-              <h1>{props.dataset.data1}</h1>
-            </Col>
-            <Col className="with-border value-space">
-              <h1>{props.dataset.data2}</h1>
-            </Col>
-          </Row>
-          <Row>
-            <Col className="with-border value-space">
-              <h1>{props.dataset.data3}</h1>
-            </Col>
-            <Col className="with-border value-space">
-              <h1>{props.dataset.data4}</h1>
-            </Col>
-          </Row>
-        </Col>
-      </Row>       
-        
-
-      </div>
-    </div>
-
-    
-  );
+    <Coin 
+      {...props}
+      prefix={'Ethereum'}
+    />)
 }
 
 export default Ethereum;
