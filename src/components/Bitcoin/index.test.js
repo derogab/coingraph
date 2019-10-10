2411564264bad75fdcfeddcc746185a4e717f73e
Riccardo Di Benedetto <dibe.ricky@gmail.com>
2019-10-10 22:25:04 +0200

    minor fix

--- src/components/Bitcoin/index.test.js
+++ src/components/Bitcoin/index.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Bitcoin from './Bitcoin';
+import Bitcoin from './';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
