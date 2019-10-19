7f3e93b7e81055352509f86ca687e1731d7f4268
Gabriele De Rosa <derosagabriele@outlook.it>
2019-10-19 17:55:36 +0200

    socket.io
    Co-authored-by: Riccardo Di Benedetto <dibe.ricky@gmail.com>

--- daemon/routes/socket.js
+++ daemon/routes/socket.js
@@ -1,5 +1,12 @@
 module.exports = function(db, config) {
-    
-    
+
+    const io = require('socket.io')()
+    const PORT = config.server.port || 8000
+
+    io.on('connection', (client) => {
+        console.log('CONNECTED!')
+    });
+    io.listen(PORT)
+    console.log('Listening on port ', PORT)
     
 };
--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import io from 'socket.io-client'
+
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const SOCKET_PORT = 8000
+
+const socketIo = io(`http://localhost:${SOCKET_PORT}`)
+
+ReactDOM.render(<App socket={socketIo} />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
