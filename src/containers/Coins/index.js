8a569bde110d1b7c22f2c6cb7893d3a77977ad2c
Gabriele De Rosa <derosagabriele@outlook.it>
2019-11-03 14:27:15 +0100

    graph limit data range

--- src/containers/Coins/index.js
+++ src/containers/Coins/index.js
@@ -36,9 +36,23 @@ export default class CoinsContainer extends Component {
 
     onNewData = (value) => {
         console.log('Received', value)
+
+        const now = new Date(Date.now());
+        const one_week_ago = new Date();
+              one_week_ago.setDate(one_week_ago.getDate() - 7);
+
         const {coinsData} = this.state
         const newData = {
-            graph: get(coinsData, [value.id, 'graph'], []).concat(value.graph),
+            graph: (get(coinsData, [value.id, 'graph'], []).concat(value.graph)).filter((element) => {
+                // only one week data show
+                var ttd = new Date(element.time * 1000); // time of this data
+                
+                if(ttd >= one_week_ago && ttd <= now) {
+                    return true;
+                }
+                
+                return false;
+            }),
             data: {
                 'data1': value.price_usd,
                 'data2': value.percent_change_1h,
