84812005d765005796218cdaec0f4ec4bd12441d
Gabriele De Rosa <derosagabriele@outlook.it>
2019-11-24 14:57:31 +0100

    graph tooltip

--- src/components/Coin/index.js
+++ src/components/Coin/index.js
@@ -2,13 +2,33 @@ import React from 'react';
 import { Row, Col } from 'react-bootstrap';
 import PropTypes from 'prop-types'
 import {
-  LineChart, Line, ResponsiveContainer, XAxis, YAxis
+  LineChart, Line, ResponsiveContainer, XAxis, YAxis, Tooltip
 } from 'recharts';
 
 import { CashData, PercentageData } from './ColumnData'
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css'
+
+const CustomTooltip = ({ active, payload, label }) => {
+  if (active) {
+    var price = payload[0].value;
+    var time = label;
+      var ts = new Date(time * 1000);
+
+    return (
+      <div className="custom-tooltip">
+        <p>
+          The value was
+          <h4>{`$${price.toFixed(2)}`}</h4>
+          on {`${ts.toUTCString()}`}
+        </p>
+      </div>
+    );
+  }
+
+  return null;
+};
 
 function Coin({datagraph, dataset, prefix}) {
 
@@ -23,8 +43,9 @@ function Coin({datagraph, dataset, prefix}) {
           <ResponsiveContainer>
             <LineChart margin={{ top: 0, right: 0, left: 0, bottom: 0 }} data={datagraph}>
               <XAxis dataKey='time' hide={true} domain={['auto', 'auto']} scale='time' type='number'/>
-              <YAxis dataKey='price' hide={false} domain={['auto', 'auto']} unit='$' />
+              <YAxis dataKey='price' hide={false} domain={['auto', 'auto']} />
               <Line type='monotone' dataKey='price' stroke='#ecf0f1' strokeWidth={2} dot={false} isAnimationActive={false} />
+              <Tooltip content={<CustomTooltip />} />
             </LineChart>
           </ResponsiveContainer>
         </Col>
