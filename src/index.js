3e72742d616ba3cbb6269ab668f68ed62cc74fae
Gabriele De Rosa <derosagabriele@outlook.it>
2020-02-28 01:52:12 +0100

    client .env

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import io from 'socket.io-client'
+import io from 'socket.io-client';
+import dotenv from 'dotenv';
 
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-const SOCKET_PORT = 8081
-const socketIo = io(`http://localhost:${SOCKET_PORT}`)
+dotenv.config();
+
+const SOCKET_PORT = process.env.REACT_APP_DAEMON_SOCKET_PORT || 8081;
+const socketIo = io(`http://localhost:${SOCKET_PORT}`);
 
 ReactDOM.render(<App socket={socketIo} />, document.getElementById('root'));
 
