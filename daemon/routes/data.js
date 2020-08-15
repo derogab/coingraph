121267b86a8a785fe254f74cc8df69c9e4e8c596
Gabriele De Rosa <derosagabriele@outlook.it>
2020-08-15 12:41:19 +0200

    Change Open API
    coinmarketcap --> coingecko

--- daemon/routes/data.js
+++ daemon/routes/data.js
@@ -20,7 +20,7 @@ module.exports = function(db, io, app, config) {
         })
         .then(function (response) {
 
-            var data = response.data[0];           
+            var data = response.data;          
 
             // save on db
             var handler = db.get('cryptocurrencies').find({'id': crypto});
@@ -29,11 +29,11 @@ module.exports = function(db, io, app, config) {
             handler.assign({name: data.name}).write();
             handler.assign({symbol: data.symbol}).write();
             handler.value().data.push({
-                "price_usd": parseFloat(data.price_usd),
-                "percent_change_1h": parseFloat(data.percent_change_1h),
-                "percent_change_24h": parseFloat(data.percent_change_24h),
-                "percent_change_7d": parseFloat(data.percent_change_7d),
-                "last_updated": parseInt(data.last_updated)
+                "price_usd": parseFloat(data.market_data.current_price.usd),
+                "percent_change_1h": parseFloat(data.market_data.price_change_percentage_1h_in_currency.usd),
+                "percent_change_24h": parseFloat(data.market_data.price_change_percentage_24h),
+                "percent_change_7d": parseFloat(data.market_data.price_change_percentage_7d),
+                "last_updated": new Date(data.market_data.last_updated).getTime()
             });
             db.write();
 
@@ -42,15 +42,15 @@ module.exports = function(db, io, app, config) {
                 'id': data.id,
                 'name': data.name,
                 'symbol': data.symbol,
-                'price_usd': parseFloat(data.price_usd),
-                'percent_change_1h': parseFloat(data.percent_change_1h),
-                'percent_change_24h': parseFloat(data.percent_change_24h),
-                'percent_change_7d': parseFloat(data.percent_change_7d),
-                'last_updated': parseInt(data.last_updated),
+                'price_usd': parseFloat(data.market_data.current_price.usd),
+                'percent_change_1h': parseFloat(data.market_data.price_change_percentage_1h_in_currency.usd),
+                'percent_change_24h': parseFloat(data.market_data.price_change_percentage_24h),
+                'percent_change_7d': parseFloat(data.market_data.price_change_percentage_7d),
+                'last_updated': parseInt(data.market_data.last_updated),
                 'graph': { // new data
                     'name': '', 
-                    'price': parseFloat(data.price_usd), 
-                    'time': parseInt(data.last_updated)
+                    'price': parseFloat(data.market_data.current_price.usd), 
+                    'time': new Date(data.market_data.last_updated).getTime()
                 }
             };
             io.emit('realtime-data', realtime_data);
