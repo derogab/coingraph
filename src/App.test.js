13221f38d59731d910e8fc310ac0581744a7c5ef
Gabriele De Rosa <derosagabriele@outlook.it>
2021-05-27 23:23:45 +0200

    Fix tests

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,23 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 it('renders without crashing', () => {
+
+  // "Official" workaround
+  // https://stackoverflow.com/questions/39830580/jest-test-fails-typeerror-window-matchmedia-is-not-a-function
+  Object.defineProperty(window, 'matchMedia', {
+    writable: true,
+    value: jest.fn().mockImplementation(query => ({
+      matches: false,
+      media: query,
+      onchange: null,
+      addListener: jest.fn(), // Deprecated
+      removeListener: jest.fn(), // Deprecated
+      addEventListener: jest.fn(),
+      removeEventListener: jest.fn(),
+      dispatchEvent: jest.fn(),
+    })),
+  });
+
   const socket = {
     on: jest.fn()
   }
