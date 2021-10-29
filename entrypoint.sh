f0577d40ab79cc041c65c883553a04b1a16065d3
Gabriele De Rosa <4183824+derogab@users.noreply.github.com>
2021-10-29 19:48:17 +0200

    Replace ENVs only in main static file

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 echo "Starting coingraph client..."
 
 # Replace ENVs in static files
-for filename in /usr/share/nginx/html/static/js/*.js; do
+for filename in /usr/share/nginx/html/static/js/main*.js; do
     echo "Replacing ENVs in $filename ..."
     sed -i "s|http://localhost|$REACT_APP_DAEMON_SOCKET_URL|g" $filename
     sed -i "s|8081|$REACT_APP_DAEMON_SOCKET_PORT|g" $filename
