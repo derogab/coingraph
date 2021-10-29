52f84770e8b259c53c33e840ee8c16c776b82878
Gabriele De Rosa <4183824+derogab@users.noreply.github.com>
2021-10-29 18:54:15 +0200

    Fix sed expression

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,8 +4,8 @@ echo "Starting coingraph client..."
 # Replace ENVs in static files
 for filename in /usr/share/nginx/html/static/js/*.js; do
     echo "Replacing ENVs in $filename ..."
-    sed -i "s/http:\/\/localhost/$REACT_APP_DAEMON_SOCKET_URL/" $filename
-    sed -i "s/8081/$REACT_APP_DAEMON_SOCKET_PORT/" $filename
+    sed -i "s|http://localhost|$REACT_APP_DAEMON_SOCKET_URL|g" $filename
+    sed -i "s|8081|$REACT_APP_DAEMON_SOCKET_PORT|g" $filename
 done
 
 echo "Coingraph client is ready!"
