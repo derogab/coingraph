602c37e25c205f68570930ef24ec7b791e9a4a44
Gabriele De Rosa <4183824+derogab@users.noreply.github.com>
2021-10-29 11:16:02 +0200

    Bug fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,9 +2,9 @@
 
 # Replace ENVs in static files
 for filename in /usr/share/nginx/html/static/js/*.js; do
-    sed -i 's/{{url}}/$REACT_APP_DAEMON_SOCKET_URL/' filename
-    sed -i 's/{{port}}/$REACT_APP_DAEMON_SOCKET_PORT/' filename
+    sed -i 's/{{url}}/$REACT_APP_DAEMON_SOCKET_URL/' $filename
+    sed -i 's/{{port}}/$REACT_APP_DAEMON_SOCKET_PORT/' $filename
 done
 
 # Run nginx 
-nginx -g daemon off;
+nginx -g 'daemon off;'
